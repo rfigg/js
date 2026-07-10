030c1abd8c9c8b17c4c175f9c97dba9d39d50889
Ryan Figg <ryan.figg@gmail.com>
2026-07-10 18:52:37 -0500

    Finished section 16 challenge 2

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -276,7 +276,7 @@ TEST COORDINATES 2: -33.933, 18.474
 
 GOOD LUCK 😀
 */
-
+/*
 const whereAmI = function (lat, lng) {
   //   console.log(lat, lng);
 
@@ -325,3 +325,222 @@ const whereAmI = function (lat, lng) {
 whereAmI(52.508, 13.381);
 whereAmI(19.037, 72.873);
 whereAmI(-33.933, 18.474);
+*/
+
+///////////////////////////////////////
+// Event Loop in Practice
+/*
+console.log('Start timer');
+setTimeout(() => console.log('0 second timer'), 0);
+Promise.resolve('Resolved promise 1').then(res => console.log(res));
+
+Promise.resolve('Resolved promise 2').then(res => {
+  for (let i = 0; i < 1000000000; i++) {}
+  console.log(res);
+});
+
+console.log('Test end');
+*/
+
+///////////////////////////////////////
+// Building a Simple Promise
+/*
+const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log('Lottery draw is happening!!!!');
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve('You win!');
+    } else {
+      reject(new Error('You lost your money :('));
+    }
+  }, 2000);
+});
+
+lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+// Promisifying setTimeout
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    // don't need reject
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+// chaining while avoiding callback hell
+wait(1)
+  .then(() => {
+    console.log('1 second passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('2 seconds passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('3 seconds passed');
+    return wait(1);
+  })
+  .then(() => console.log('4 seconds passed'));
+
+// setTimeout(() => {
+//   console.log('1 second passed');
+//   setTimeout(() => {
+//     console.log('2 seconds passed');
+//     setTimeout(() => {
+//       console.log('3 seconds passed');
+//       setTimeout(() => {
+//         console.log('4 seconds passed');
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+
+Promise.resolve('abc').then(x => console.log(x));
+Promise.reject(new Error('problem!')).catch(x => console.error(x));
+*/
+
+///////////////////////////////////////
+// promisifying geolocation
+
+// navigator.geolocation.getCurrentPosition(
+//   position => console.log(position),
+//   err => console.error(err),
+// );
+// console.log('Getting position');
+/*
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    // navigator.geolocation.getCurrentPosition(
+    //   position => resolve(position),
+    //   err => reject(err),
+    // );
+    // same as above
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+
+getPosition().then(pos => console.log(pos));
+//   .catch(err => console.error('NO WAY!'));
+
+// promisify function from earlier exercise
+const whereAmI = function () {
+  getPosition()
+    .then(pos => {
+      const { latitude: lat, longitude: lng } = pos.coords;
+
+      return fetch(
+        `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`,
+      );
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Problem with geocoding: ${response.status}`);
+      }
+
+      return response.json();
+    })
+    .then(data => {
+      console.log(`You are in ${data.city}, ${data.countryName}`);
+
+      return fetch(
+        `https://countries-api-836d.onrender.com/countries/alpha/${data.countryCode}`,
+      );
+    })
+    .then(response => {
+      if (!response.ok)
+        throw new Error(`Country not found (${response.status})`);
+      return response.json();
+    })
+    .then(data => {
+      renderCountry(data);
+    })
+    .catch(err => {
+      console.error(`Uh oh! Error: ${err.message}`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
+};
+
+btn.addEventListener('click', whereAmI);
+*/
+
+///////////////////////////////////////
+// Challenge  #2
+/* 
+Build the image loading functionality that I just showed you on the screen.
+
+Tasks are not super-descriptive this time, so that you can figure out some stuff on your own. Pretend you're working on your own 😉
+
+PART 1
+1. Create a function 'createImage' which receives imgPath as an input. This function returns a promise which creates a new image (use document.createElement('img')) and sets the .src attribute to the provided image path. When the image is done loading, append it to the DOM element with the 'images' class, and resolve the promise. The fulfilled value should be the image element itself. In case there is an error loading the image ('error' event), reject the promise.
+
+If this part is too tricky for you, just watch the first part of the solution.
+
+PART 2
+2. Comsume the promise using .then and also add an error handler;
+3. After the image has loaded, pause execution for 2 seconds using the wait function we created earlier;
+4. After the 2 seconds have passed, hide the current image (set display to 'none'), and load a second image (HINT: Use the image element returned by the createImage promise to hide the current image. You will need a global variable for that 😉);
+5. After the second image has loaded, pause execution for 2 seconds again;
+6. After the 2 seconds have passed, hide the current image.
+
+TEST DATA: Images in the img folder. Test the error handler by passing a wrong image path. Set the network speed to 'Fast 3G' in the dev tools Network tab, otherwise images load too fast.
+
+GOOD LUCK 😀
+*/
+let currentImg;
+
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const createImage = function (imgPath) {
+  //   console.log(imgPath);
+  return new Promise(function (resolve, reject) {
+    const newImg = document.createElement('img');
+
+    newImg.addEventListener('load', function () {
+      //   console.log('loaded');
+      // append to element with images class, resolve promise
+      document.querySelector('.images').append(newImg);
+      // he does the above query as a global, consistent with rest of course
+      resolve(newImg);
+    });
+
+    newImg.addEventListener('error', function () {
+      //   console.log("couldn't load");
+      reject(new Error("Couldn't load the dang image!"));
+    });
+
+    newImg.src = imgPath; // he does this before event listeners. why?
+  });
+};
+
+createImage('img/img-1.jpg')
+  .then(img => {
+    // copied his img instead of res
+    console.log(img);
+    currentImg = img;
+    return wait(2);
+  })
+  .then(() => {
+    console.log('waited 2');
+    currentImg.style.display = 'none';
+    return createImage('img/img-2.jpg');
+  })
+  .then(img => {
+    console.log(img);
+    currentImg = img;
+    return wait(2);
+  })
+  .then(() => {
+    console.log('waited 2 more');
+    currentImg.style.display = 'none';
+  })
+  .catch(err => console.error(err)); // copied his .error instead of .log
+
+// createImage('img/img-1.jpgggggg')
+//   .then(res => console.log(res))
+//   .catch(err => console.log(err));
