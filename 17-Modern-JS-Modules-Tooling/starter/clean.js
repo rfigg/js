1044c51250682c8e0b69722b8adb56f96c9b8089
Ryan Figg <ryan.figg@gmail.com>
2026-08-15 12:08:52 -0500

    Finished section 17

--- 17-Modern-JS-Modules-Tooling/starter/clean.js
+++ 17-Modern-JS-Modules-Tooling/starter/clean.js
@@ -11,18 +11,14 @@ const budget = Object.freeze([
   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
 ]);
 
-// budget[0].value = 10000;
-// budget[8] = 'jonas';
-
 const spendingLimits = Object.freeze({
   jonas: 1500,
   matilda: 100,
 });
-// spendingLimits.jay = 200;
-// spendingLimits.jonas = 1600;
+
 console.log(spendingLimits);
 
-const getLimit = user => spendingLimits?.[user] ?? 0;
+const getLimit = (limits, user) => limits?.[user] ?? 0;
 
 // now a Pure function
 const addExpense = function (
@@ -32,29 +28,9 @@ const addExpense = function (
   description,
   user = 'jonas',
 ) {
-  // if (!user) user = 'jonas';
   const cleanUser = user.toLowerCase();
 
-  // let lim;
-  // if (spendingLimits[user]) {
-  //   lim = spendingLimits[user];
-  // } else {
-  //   lim = 0;
-  // }
-
-  // same with ternary instead of if-else blocks and const instead of mutation
-  // const limit = spendingLimits[user] ? spendingLimits[user] : 0;
-
-  // now with optional chaining and nullish coalescing
-  // const limit = spendingLimits?.[user] ?? 0;
-  // const limit = getLimit(user);
-
-  // if (value <= getLimit(cleanUser)) {
-  //   // budget.push({ value: -value, description: description, user: user }); // enhanced object literal
-  //   // budget.push({ value: -value, description, user: cleanUser });
-  //   return [...state, { value: -value, description, user: cleanUser }];
-  // }
-  return value <= getLimit(cleanUser)
+  return value <= getLimit(limits, cleanUser)
     ? [...state, { value: -value, description, user: cleanUser }]
     : state;
 };
@@ -68,38 +44,43 @@ const newBudget2 = addExpense(
 );
 const newBudget3 = addExpense(newBudget2, spendingLimits, 200, 'Stuff', 'Jay');
 
-console.log(newBudget1);
-console.log(newBudget2);
-console.log(newBudget3);
+// console.log(newBudget1);
+// console.log(newBudget2);
 
-const checkExpenses = function () {
-  for (const entry of budget)
-    // let lim;
-    // if (spendingLimits[entry.user]) {
-    //   lim = spendingLimits[entry.user];
-    // } else {
-    //   lim = 0;
-    // }
-    // const limit = spendingLimits?.[entry.user] ?? 0;
-    // const limit = getLimit(entry.user);
+// const checkExpensesOriginal = function (state, limits) {
+//   return state.map(entry => {
+//     return entry.value < -getLimit(limits, entry.user)
+//       ? { ...entry, flag: 'limit' }
+//       : entry;
+//   });
+//   // for (const entry of state)
+//   //   if (entry.value < -getLimit(limits, entry.user)) entry.flag = 'limit';
+// };
 
-    if (entry.value < -getLimit(entry.user)) entry.flag = 'limit';
+const checkExpenses = (state, limits) =>
+  state.map(entry =>
+    entry.value < -getLimit(limits, entry.user)
+      ? { ...entry, flag: 'limit' }
+      : entry,
+  );
+const finalBudget = checkExpenses(newBudget3, spendingLimits);
+
+console.log(finalBudget);
+
+const logBigExpenses = function (state, bigLimit) {
+  const bigExpenses = state
+    .filter(entry => entry.value <= -bigLimit)
+    .map(entry => entry.description.slice(-2))
+    .join(' / ');
+  // .reduce((str, cur) => `${str} / ${cur.description.slice(-2)}`, '');
+  console.log(bigExpenses);
+
+  // let output = '';
+  // for (const entry of budget) //{
+  //   output +=
+  //     entry.value <= -bigLimit ? `${entry.description.slice(-2)} / ` : '';
+  // output = output.slice(0, -2); // Remove last '/ '
+  // console.log(output);
 };
-checkExpenses();
 
-const logBigExpenses = function (bigLimit) {
-  let output = '';
-  for (const entry of budget) //{
-    output +=
-      entry.value <= -bigLimit ? `${entry.description.slice(-2)} / ` : '';
-  // if (entry.value <= -bigLimit) {
-  // output += entry.description.slice(-2) + ' / '; // Emojis are 2 chars
-  // output += `${entry.description.slice(-2)} / `;
-  // }
-  //}
-  output = output.slice(0, -2); // Remove last '/ '
-  console.log(output);
-};
-
-console.log(budget);
-logBigExpenses(500);
+logBigExpenses(finalBudget, 500);
